72e41ea527950923536daadb9484576aeff0bf36
Troy Kearney <tkearney18@gmail.com>
2017-03-17 15:17:06 -0400

    Added comment to index.js to state it's purpose

---
+++ src/index.js
@@ -0,0 +1,1 @@
+// This is where the root of the application will live
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,38 @@
-import webpack from 'webpack';
-import path from 'path';
+import webpack from 'webpack'; // importing the module bundler
+import path from 'path'; // path comes from NodeJS
 
+// for more info on the webpack configuration, go to https://webpack.github.io/docs/configuration.html
 export default {
-  devtool: 'inline-source-map',
+  // developer tool for debugging
+  devtool: 'inline-source-map', // A SourceMap is added as DataUrl to the JavaScript file
+  // entry point for our application
   entry: [
-    'webpack-hot-middleware/client',
-    path.resolve(__dirname, 'src/index')
+    'webpack-hot-middleware/client', // the Hot Module Replacement for express
+    path.resolve(__dirname, 'src/index.js') // index.js is the entry point to our application
   ],
-  target: 'web',
+  // what compile method should webpack aim for
+  target: 'web', // this is the default
+  // options for the output file
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
     filename: 'bundle.js'
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'src')
-  },
+  // This is not needed. this would be used for webpack-dev-server.
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, 'src')
+  // },
+  // adding addtional plugins to the compiler
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   ],
+  // options affecting the nomal modules
   module: {
+    // loaders allow for preprocessing of files
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] }, // preprocessing the .js files with babel-loader
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] } // preprocessing css files
     ]
   }
 };
