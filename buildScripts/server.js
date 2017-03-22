7009c34b6b2fe2d3ae9a59890521af29d93b625b
Troy Kearney <tkearney18@gmail.com>
2017-03-22 09:41:21 -0400

    Added comments for the server

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -2,12 +2,13 @@ import express from 'express';
 import webpack from 'webpack';
 import path from 'path';
 import open from 'open';
-import config from '../webpack.config.dev';
+import config from '../webpack.config.dev'; // exporting all configurations from webpack.config.dev.js
 
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+// setting up the middleware to be use by express
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
@@ -15,15 +16,17 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+// for all incoming requests serve up the index.html page
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+// have the express server start listening on the set port
 app.listen(port, function (err) {
     if (err) {
         console.log(err);
     } else {
-        open(`http://localhost:${port}`);
+        open(`http://localhost:${port}`); // this will open up the browser to this location
         //console.log(`http://localhost:${port}`);
     }
 });
